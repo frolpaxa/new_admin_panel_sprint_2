566398ead124c13a78a75c0d4cb731956bd5ae3b
Paul <01fpv@mail.ru>
2024-01-16 10:17:08 +0300

    complete =)

--- docker_compose/empty_project/app/run_uwsgi.sh
+++ docker_compose/empty_project/app/run_uwsgi.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-if [ ! -f /created ]
-    python manage.py collectstatic --noinput && \
-    yes | python manage.py makemigrations --merge && \
-    python manage.py migrate --fake movies 0001 --noinput && \
-    python manage.py migrate --noinput && \
-    python manage.py createsuperuser --noinput || echo superuser exist && \
-    python /mnt/sqlite_to_postgres/load_data.py
-    touch /created
-fi
+python manage.py collectstatic --noinput && \
+yes | python manage.py makemigrations --merge && \
+python manage.py migrate --fake movies 0001 --noinput && \
+python manage.py migrate --noinput && \
+python manage.py createsuperuser --noinput || echo superuser exist && \
+python /mnt/sqlite_to_postgres/load_data.py
 
 chown www-data:www-data /var/log
 
