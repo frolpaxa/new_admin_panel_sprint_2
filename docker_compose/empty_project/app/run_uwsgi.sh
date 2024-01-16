034bdda3d31a243e9bc4a1ffe328f7d7b5a1a7c0
Paul <01fpv@mail.ru>
2024-01-16 10:07:45 +0300

    fix check

--- docker_compose/empty_project/app/run_uwsgi.sh
+++ docker_compose/empty_project/app/run_uwsgi.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-FILE=/opt/app/created
-
-if [ -f "$FILE" ]; then
+if [ ! -f /created ]
     python manage.py collectstatic --noinput && \
     yes | python manage.py makemigrations --merge && \
     python manage.py migrate --fake movies 0001 --noinput && \
     python manage.py migrate --noinput && \
     python manage.py createsuperuser --noinput || echo superuser exist && \
     python /mnt/sqlite_to_postgres/load_data.py
-    touch /opt/app/created
+    touch /created
 fi
 
 chown www-data:www-data /var/log
